fc3cb91c24a5fa63f8e22b56300699e0ef3a8c12
Dangchan <dang11230@gmail.com>
2024-03-13 21:12:40 +0900

    comment

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -7,9 +7,9 @@ costs = [list(map(int, input().split())) for _ in range(N)]
 dp = [int(1e7) for _ in range(C+100)] # point ! : maximum N = 100 thus overing num can be 100
 dp[0] = 0
 
-for cost, cust in costs:
-    for i in range(cust,C+100):
-        dp[i] = min(dp[i - cust] + cost, dp[i])
+for cost, cust in costs: # 현재 도시에서 손님을 받는 경우
+    for i in range(cust,C+100): # point ! : C+100까지만 계산해도 충분하다. 왜냐하면, 최대 100명의 손님이 있으므로, 100명을 넘어가는 경우는 고려할 필요가 없다.
+        dp[i] = min(dp[i - cust] + cost, dp[i]) # i번째 손님까지의 최소비용 update
     
 
 print(min(dp[C:]))
