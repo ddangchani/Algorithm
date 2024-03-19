5f256418edb35fe48cca887473cf97f487ec4e95
Dangchan <59010099+ddangchani@users.noreply.github.com>
2024-03-19 22:26:01 +0900

    [Silver III] Title: N과 M (3), Time: 1996 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+
+def backtrack(ls):
+    if len(ls) == M:
+        print(*ls)
+        return
+    for i in range(1, N+1):
+        ls.append(i)
+        backtrack(ls)
+        ls.pop()
+
+   
+backtrack([])
