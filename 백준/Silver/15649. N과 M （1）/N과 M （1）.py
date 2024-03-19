2e28fa9d5f66244eed77f1e894292da0fdcf55a4
Dangchan <59010099+ddangchani@users.noreply.github.com>
2024-03-20 00:55:28 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,16 @@
 N, M = map(int, input().split())
+ans = [0] * M
+isused = [False] * (N+1)
 
-def backtrack(ls):
-    if len(ls) == M:
-        print(*ls)
-        return
-    for i in range(1, N+1):
-        if i not in ls:
-            ls.append(i)
-            backtrack(ls)
-            ls.pop()
-    
-   
-backtrack([])
+def back(n):
+    if n == M: # 배열의 길이를 확인
+        print(*ans)
+        return 
+    for i in range(1, N+1): # 1 ~ N 까지
+        if not isused[i]: # O(1)로 변환가능
+            isused[i] = True
+            ans[n] = i 
+            back(n+1)
+            isused[i] = False
+
+back(0)
