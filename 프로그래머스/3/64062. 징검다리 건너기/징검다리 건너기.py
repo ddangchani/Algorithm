42a569b4434cac1f6ab6a04a7d6e882b8eb3cf30
Dangchan <59010099+ddangchani@users.noreply.github.com>
2024-03-19 19:34:02 +0900

    [level 3] Title: 징검다리 건너기, Time: 170.58 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,20 +1,9 @@
 # len 10^5 > Two pointer(sliding window)
-## Find minimum of max value in each window
-from heapq import *
 
 def solution(stones, k):
-    # k becomes window size
-    q = [((-1) * val, idx) for idx, val in enumerate(stones[:k])]
-    heapify(q)
-    # initial max value of window
-    cur = q[0][0] * (-1)
+    answer = max(stones[:k])
+    # window size k
+    for i in range(len(stones)-k):
+        answer = min(answer, max(stones[i:i+k]))
     
-    for i in range(k, len(stones)):
-        
-        # move window
-        heappush(q, (stones[i] * (-1), i))
-        while q[0][1] <= i-k: # NOTE : if current max cannot be acquired from current window
-            heappop(q)
-        cur = min(cur, q[0][0] * (-1))
-    
-    return cur
+    return answer
